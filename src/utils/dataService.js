394f84da95653f11f759b97decc288fa2971c74c
Ejay02 <e.jae02@gmail.com>
2024-11-15 15:50:49 +0100

    fix map dispaly production debug

--- src/utils/dataService.js
+++ src/utils/dataService.js
@@ -1,11 +1,6 @@
 export async function fetchMillData() {
-  try {
-    const response = await fetch("/src/assets/milljson.json");
-    if (!response.ok) throw new Error("Failed to load mill data");
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    notify("Error loading mill data", "error");
-    throw error;
-  }
+  const response = await fetch("/src/assets/milljson.json");
+  if (!response.ok) throw new Error("Failed to load mill data");
+  const data = await response.json();
+  return data;
 }
